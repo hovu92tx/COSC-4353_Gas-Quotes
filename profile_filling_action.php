67141905e4e24f41499d98644f5575c9b7f1a9db
don3403 <126264495+don3403@users.noreply.github.com>
2023-02-24 20:51:50 -0600

    Update profile_filling_action.php

--- profile_filling_action.php
+++ profile_filling_action.php
@@ -9,4 +9,18 @@ try {
             $address1 = $_POST['address1'];
             if ($_POST['address2'] === '') {
                 $address2 = '0';
- 
+            } else {
+                $address2 = $_POST['address2'];
+            }
+            $city = $_POST['city'];
+            $state = $_POST['state'];
+            $zipcode = $_POST['zipcode'];
+            $sql = "UPDATE `user_profiles` SET name= '$name', address1='$address1', address2= '$address2', city= '$city',state= '$state', zipcode= '$zipcode' WHERE userid ='$user_id'";
+            $conn->query($sql);
+            $_SESSION['loction'] = $city;
+            header('location: dashboard.php');
+        }
+    }
+} catch (PDOException $error) {
+    echo $error;
+}
