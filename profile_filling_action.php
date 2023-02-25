e34096dd90f5c06bfc8632e38cd53e9ab127c17c
don3403 <126264495+don3403@users.noreply.github.com>
2023-02-24 20:50:23 -0600

    Update profile_filling_action.php

--- profile_filling_action.php
+++ profile_filling_action.php
@@ -2,3 +2,11 @@
 session_start();
 require 'connect.php';
 try {
+    if (isset($_POST["pf_submit_button"])) {
+        if (!empty($_POST['name'] && $_POST['address1'] && $_POST['city'] && $_POST['state'] && $_POST['zipcode'])) {
+            $user_id = $_SESSION['userid'];
+            $name = $_POST['name'];
+            $address1 = $_POST['address1'];
+            if ($_POST['address2'] === '') {
+                $address2 = '0';
+ 
