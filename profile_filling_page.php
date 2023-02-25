c28b28a813933ae39e1bde63a0fa3d686552a92d
don3403 <126264495+don3403@users.noreply.github.com>
2023-02-24 20:45:47 -0600

    Update profile_filling_page.php

--- profile_filling_page.php
+++ profile_filling_page.php
@@ -4,3 +4,14 @@ $_SESSION['login_status'] = '0';
 $_SESSION['username'] = '';
 $_SESSION['user_id'] = '';
 error_reporting(0);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ABC Company</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="CSS/profile_filling_page.css">
+</head>
