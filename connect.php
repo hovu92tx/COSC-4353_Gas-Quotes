0ef259afbc9dfc736fc6eb95f74c681ddbbe54b7
vickym22 <124326261+vickym22@users.noreply.github.com>
2023-04-27 18:51:44 -0500

    V 4.0.2

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root";
-$password = "User123*";
+$password = "";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=abc database", $username, $password);
