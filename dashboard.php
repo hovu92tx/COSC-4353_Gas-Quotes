6259c0beec58ca99765829396ece999b9b1cfd9b
vickym22 <124326261+vickym22@users.noreply.github.com>
2023-04-28 00:34:15 -0500

    V 4.0.1

--- dashboard.php
+++ dashboard.php
@@ -13,3 +13,21 @@ date_default_timezone_set('America/Chicago');
     <?php echo $head ?>
 </head>
 
+<body>
+    <header>
+        ABC Company
+    </header>
+    <!--Menu Bar-->
+    <aside>
+        <?php menu('dash_board') ?>
+    </aside>
+    <!--Quotes-->
+    <section>
+        <?php dash_quote() ?>
+        <div><a id="button_link" style="display: block; margin: auto;" href="cart.php">Go To Cart</a></div>
+    </section>
+</body>
+<footer>
+</footer>
+
+</html>
